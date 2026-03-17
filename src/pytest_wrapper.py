9f7d08edc8d98e2df7c28f0d577d1182c3b76ba5
zh3w4ng <zh3w4ng@gmail.com>
2026-03-17 11:15:58 -0700

    add .coveragerc

--- src/pytest_wrapper.py
+++ src/pytest_wrapper.py
@@ -6,5 +6,11 @@ if __name__ == "__main__":
     # Add pytest-cov arguments if not already provided
     args = sys.argv[1:]
     if "--cov" not in args:
-        args.extend(["--cov=.", "--cov-report=term-missing"])
+        args.extend(
+            [
+                "--cov=.",
+                "--cov-report=term-missing",
+                "--cov-config=src/.coveragerc",
+            ]
+        )
     sys.exit(pytest.main(args))
